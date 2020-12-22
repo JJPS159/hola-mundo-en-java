32f4b29d989c448696e732fe989c8abb97224d1f
JJPS159 <jjps195@gmail.com>
2020-12-22 12:58:23 +0100

    Saludo de colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -9,7 +9,8 @@ public class HolaMundo {
     public static void main(String[] arg) {
         String naranja = "\033[33m";
         String azul = "\033[34m";
-        System.out.println("¡Hola Mundo y Mas!");
-        System.out.println("¡Aquí estamos haciendo pruebas con Git.!");
+
+        System.out.println(naranja +"¡Hola Mundo y Mas!");
+        System.out.println(azul + "¡Aquí estamos haciendo pruebas con Git.!");
     }
 }
