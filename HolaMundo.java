34e2f3385a2b5d053d8c711e4510a3cee3a2b062
JJPS159 <jjps195@gmail.com>
2020-12-22 12:45:00 +0100

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -7,6 +7,8 @@
 public class HolaMundo {
 
     public static void main(String[] arg) {
+        String naranja = "\033[33m";
+        String azul = "\033[34m";
         System.out.println("¡Hola Mundo y Mas!");
         System.out.println("¡Aquí estamos haciendo pruebas con Git.!");
     }
